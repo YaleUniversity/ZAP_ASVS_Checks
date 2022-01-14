06c381a08abe5cc9ed504fce1e700c231c6dd6be
colbee <laracuente.colby@gmail.com>
2022-01-14 10:51:44 -0500

    edits to content-type script

--- ZAP_Scripts/passive/14-4-1-content-type-header.py
+++ ZAP_Scripts/passive/14-4-1-content-type-header.py
@@ -1,29 +1,46 @@
 """
 
-Script testing 3.4.1 control from OWASP ASVS 4.0:
-'Verify that cookie-based session tokens have the 'Secure' attribute set.'
+Script testing 14.4.1 control from OWASP ASVS 4.0:
+'Verify that every HTTP response contains a Content-Type header. Also
+specify a safe character set (e.g., UTF-8, ISO-8859-1) if the content types are
+text/*, /+xml and application/xml. Content must match with the provided
+Content-Type header.'
 
-The script will raise an alert if 'Secure' attribute is not present. 
+The script will raise an alert if:
+	1. There is no Content-Type header in the HTTP response
+	2. The content types are text/*, /+xml and application/xml but safe character sets: UTF-8, ISO-8859-1 are not used
 
 """
+def useSafeCharacters(header):
+  types = ["text/", "/+xml", "application/xml"]
+  for t in types:
+    if (t in header and (("charset=utf-8" not in header) or "ISO-8859-1" not in header)):
+      return True
+  return False
 
 def scan(ps, msg, src):
 
-  headerCookie = str(msg.getResponseHeader().getHeader("Set-Cookie"))
+  header = str(msg.getResponseHeader().getHeader("Content-Type"))
 
   alertRisk= 0
   alertConfidence = 1
-  alertTitle = "3.4.1 Verify that cookie-based session tokens have the 'Secure' attribute set."
-  alertDescription = "The secure attribute is an option that can be set by the application server when sending a new cookie to the user within an HTTP Response. The purpose of the secure attribute is to prevent cookies from being observed by unauthorized parties due to the transmission of the cookie in clear text. To accomplish this goal, browsers which support the secure attribute will only send cookies with the secure attribute when the request is going to an HTTPS page. Said in another way, the browser will not send a cookie with the secure attribute set over an unencrypted HTTP request. By setting the secure attribute, the browser will prevent the transmission of a cookie over an unencrypted channel."
+  alertTitle = "14.4.1 Verify that every HTTP response contains a Content-Type header. Also specify a safe character set (e.g., UTF-8, ISO-8859-1) if the content types are text/*, /+xml and application/xml. Content must match with the provided Content-Type header."
+  alertDescription = "The Content-Type representation header is used to indicate the original media type of the resource (before any content encoding is applied for sending)."
   url = msg.getRequestHeader().getURI().toString()
   alertParam = ""
   alertAttack = ""
-  alertInfo = "https://owasp.org/www-community/controls/SecureCookieAttribute"
-  alertSolution = "Add 'Secure' attribute when sending cookie."
-  alertEvidence = "" 
-  cweID = 614
+  alertInfo = "https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html"
+  solutions = ["Add 'Content-Type' header in HTTP response.", "Specify a safe character set (e.g., UTF-8, ISO-8859-1) if the content types are text/*, /+xml and application/xml"]
+  alertSolution = ""
+  alertEvidence = "Content-Type: " + header
+  cweID = 173
   wascID = 0
   
-  if (headerCookie != "None" and "secure" not in headerCookie.lower()):
+  if (header == "None"):
+    alertSolution = solutions[0]
+  elif (useSafeCharacters(header)):
+    alertSolution = solutions[1]
+
+  if (alertSolution != ""):
     ps.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDescription, 
       url, alertParam, alertAttack, alertInfo, alertSolution, alertEvidence, cweID, wascID, msg);
