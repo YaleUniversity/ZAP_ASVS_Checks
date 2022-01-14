2341aea87ebd5d06cf1a976d689313a15ece6d7b
colbee <laracuente.colby@gmail.com>
2022-01-14 11:28:28 -0500

    edits to content-disposition script

--- ZAP_Scripts/passive/14-4-2-api-content-disposition-header.py
+++ ZAP_Scripts/passive/14-4-2-api-content-disposition-header.py
@@ -1,29 +1,31 @@
 """
 
-Script testing 3.4.1 control from OWASP ASVS 4.0:
-'Verify that cookie-based session tokens have the 'Secure' attribute set.'
+Script testing 14.4.2 control from OWASP ASVS 4.0:
+'Verify that all API responses contain a Content-Disposition: attachment;
+filename="api.json" header (or other appropriate filename for the content
+type).'
 
-The script will raise an alert if 'Secure' attribute is not present. 
+The script will raise an alert if 'Content-Disposition' header is present but not follow the format - Content-Disposition: attachment; filename=
 
 """
 
 def scan(ps, msg, src):
 
-  headerCookie = str(msg.getResponseHeader().getHeader("Set-Cookie"))
+  header = str(msg.getResponseHeader().getHeader("Content-Disposition"))
 
   alertRisk= 0
   alertConfidence = 1
   alertTitle = "3.4.1 Verify that cookie-based session tokens have the 'Secure' attribute set."
-  alertDescription = "The secure attribute is an option that can be set by the application server when sending a new cookie to the user within an HTTP Response. The purpose of the secure attribute is to prevent cookies from being observed by unauthorized parties due to the transmission of the cookie in clear text. To accomplish this goal, browsers which support the secure attribute will only send cookies with the secure attribute when the request is going to an HTTPS page. Said in another way, the browser will not send a cookie with the secure attribute set over an unencrypted HTTP request. By setting the secure attribute, the browser will prevent the transmission of a cookie over an unencrypted channel."
+  alertDescription = "Verify that all API responses contain a Content-Disposition: attachment; filename='api.json'header (or other appropriate filename for the content type)."
   url = msg.getRequestHeader().getURI().toString()
   alertParam = ""
   alertAttack = ""
-  alertInfo = "https://owasp.org/www-community/controls/SecureCookieAttribute"
-  alertSolution = "Add 'Secure' attribute when sending cookie."
+  alertInfo = "https://owasp.org/www-community/vulnerabilities/Unrestricted_File_Upload"
+  alertSolution = "Use the format 'Content-Disposition: attachment; filename=' for API responses"
   alertEvidence = "" 
-  cweID = 614
+  cweID = 116
   wascID = 0
   
-  if (headerCookie != "None" and "secure" not in headerCookie.lower()):
+  if (header != "None" and "attachment; filename=" not in header.lower()):
     ps.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDescription, 
       url, alertParam, alertAttack, alertInfo, alertSolution, alertEvidence, cweID, wascID, msg);
