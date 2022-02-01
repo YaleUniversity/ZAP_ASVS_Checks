a28d2238f1400f950e0a4a63e7051554b37cb23f
colbee <laracuente.colby@gmail.com>
2022-02-01 17:16:56 -0500

    add explination to json injection script

--- ZAP_Scripts/active/5-3-6-json-injection.py
+++ ZAP_Scripts/active/5-3-6-json-injection.py
@@ -4,12 +4,19 @@ Script testing 5.3.6 control from OWASP ASVS 4.0:
 'Verify that the application protects against JSON injection attacks, 
 JSON eval attacks, and JavaScript expression evaluation.'
 
-The script will raise an alert if 
+The scan function will check if a message returns JSON data by checking for "application/json" in the Content-Type response header.
+If it does, it will try injecting various common JSON elements like [, ], {, }, etc... that might be used in a JSON injection attack.
+The payload will include a key word along with the element. This way, when we check the response body for the payload, we can reudence 
+the number of false positives from properly formatted JSON data.
+
+The script will raise an alert if the payload is echoed in the response body (no sanitization or encoding)
 
 """
 
 def scanNode(sas, msg):
+  pass
 
+def scan(sas, msg, param, value):
   #alert parameters
   alertRisk= 0
   alertConfidence = 1
@@ -36,19 +43,18 @@ def scanNode(sas, msg):
   print(response_header)
   if ((response_header != None) and "application/json" in response_header):
     print("body ")
+    for element in common_json_elements:
+      attack = "json attack" + elment
+      # setParam (message, parameterName, newValue)
+      sas.setParam(msg, param, attack);
 
-    # setParam (message, parameterName, newValue)
-    #sas.setParam(msg, param, 'Your attack');
-
-    # sendAndReceive(msg, followRedirect, handleAntiCSRFtoken)
-    #sas.sendAndReceive(msg, False, False);
+      # sendAndReceive(msg, followRedirect, handleAntiCSRFtoken)
+      sas.sendAndReceive(msg, False, False);
 
 
-    # Test the responses and raise alerts as below
-    if (True):
-      sas.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDescription, 
-      url, alertParam, alertAttack, alertInfo, alertSolution, alertEvidence, cweID, wascID, msg);
-
-def scan(sas, msg, param, value):
-  pass
+      # Test the responses and raise alerts as below
+      if (attack in msg.getRequestBody()):
+        alertEvidence = element + " not sanatized"
+        sas.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDescription, 
+        url, alertParam, alertAttack, alertInfo, alertSolution, alertEvidence, cweID, wascID, msg);
 
