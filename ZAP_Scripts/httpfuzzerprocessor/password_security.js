23795fc7f616a16ba85c97886d28167424209aa7
colbee <laracuente.colby@gmail.com>
2022-03-08 15:56:10 -0500

    edit alert conditions

--- ZAP_Scripts/httpfuzzerprocessor/password_security.js
+++ ZAP_Scripts/httpfuzzerprocessor/password_security.js
@@ -14,15 +14,28 @@ Script testing the following password security controls from OWASP ASVS 4.0:
 2.1.9 - Verify that there are no password composition rules limiting the type of characters permitted. There should be no requirement for upper or lower case or numbers or special characters.
 
 
-This script will 
+Once the fuzzer is run with the provided wordlist, this script will check the response from each fuzzed request. 
+
+	If the reponse status code is successful (200-209), the script will check the payload and add a custom status code if following criteria apply:
+		1. Payload is less than 12 characters (2.1.1)
+		2. Payload is greater than 128 characters (2.1.2)
+		3. Payload is from top 1,000 most common passwords, taken from rockyou.txt (2.1.7)
+		*This condition is true by default if the payload is greater than 12 and less than 128 characters long. This is because the wordlist contains only payloads from rockyou.txt and payloads that are too short or too long.
+		*If changes are made to the wordlist or another one is used, false positive may be triggered so please review your results.
+	
+	If the response status code is NOT successful,  the script will check the payload and add a custom status code if following criteria apply (Note: these conditions are more prone to false positives):
+		1. Payload is between 64 and 128 characters (2.1.2)
+		2. Payload is valid length and contains special character (2.1.9)
 
 */
 
 
-var ScriptsActiveScanner = Java.type('org.zaproxy.zap.extension.ascan.ScriptsActiveScanner');
-
 // Auxiliary variables/constants needed for processing.
 var count = 1;
+function containsSpecial(str){
+	var regex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
+	return regex.test(str);
+}
 
 function processMessage(utils, message) {
 	message.getRequestHeader().setHeader("X-Unique-Id", count);
@@ -42,7 +55,7 @@ function processResult(utils, fuzzResult){
 	// To get the value of a specific configured script parameter
 	//    utils.getParameters().get("exampleParam1")
 
-     var payload = utils.getPayloads();
+     var payload = utils.getPayloads().toString();
      var codes = [200, 201, 202, 203, 204, 205, 206, 207, 208, 209];
 
      var response_code = fuzzResult.getHttpMessage().getResponseHeader().getStatusCode();
@@ -59,35 +72,32 @@ function processResult(utils, fuzzResult){
        + "If the password is breached, the application must require the user to set a new non-breached password."
        + "cweID:  " + "521"
        + "\n" + "https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/04-Authentication_Testing/07-Testing_for_Weak_Password_Policy";
-  	var url = msg.getRequestHeader().getURI().toString();
-  	var alertParam = "";
-  	var alertAttack = "";
-  	var alertInfo = "https://owasp.org/www-project-json-sanitizer/migrated_content";
-  	var alertSolution = "";
- 	var alertEvidence = "";
-  	var cweID = 830;
-  	var wascID = 0;
+     var length = payload.length;
      
      
 	if (index != -1){//if, status code is found in list of successful codes (permitted)
-          if (payload.length < 12){
-               //alertDescription = "2.1.1 - Verify that user set passwords are at least 12 characters in length (after multiple spaces are combined)."
-          }
-          if (payload.length > 128){
-               //alertDescription = "2.1.2 - Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied."
-          }
+          if (length < 12){
+               
+               fuzzResult.addCustomState("Key Custom State", "2.1.1 - Verify that user set passwords are at least 12 characters in length (after multiple spaces are combined).")
+          }else if (length > 128){
+               
+               fuzzResult.addCustomState("Key Custom State", "2.1.2 - Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied.")
+          }else{
+               fuzzResult.addCustomState("Key Custom State", "2.1.7 - Verify that passwords submitted during account registration, login, and password change are checked against a set of breached passwords" 
+        + "either locally (such as the top 1,000 or 10,000 most common passwords which match the system's password policy) or using an external API. "
+        + "If using an API a zero knowledge proof or other mechanism should be used to ensure that the plain text password is not sent or used in verifying the breach status of the password. "
+        + "If the password is breached, the application must require the user to set a new non-breached password.")
+}    
 
      }else{//if denied/error
-          if (payload.length < 12){
-               //alertDescription = "2.1.9 - Verify that there are no password composition rules limiting the type of characters permitted. There should be no requirement for upper or lower case or numbers or special characters."
+          print(length);
+          if (length > 63 && length < 129){
+			fuzzResult.addCustomState("Key Custom State", "2.1.2 - Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied.")
+		}else if ((length > 11 && length < 129) && containsSpecial(payload)){
+               fuzzResult.addCustomState("Key Custom State", "2.1.9 - Verify that there are no password composition rules limiting the type of characters permitted. There should be no requirement for upper or lower case or numbers or special characters.")
           }
-          if (payload.length > 11 && payload.length < 129){
-               //alertDescription = "2.1.2 - Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied."
 
-          }
-
-}ScriptsActiveScanner.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDescription, 
-          url, alertParam, alertAttack, alertInfo, alertSolution, alertEvidence, cweID, wascID, msg);
+     }
 	return true;
 }
 
