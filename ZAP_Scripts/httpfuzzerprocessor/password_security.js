32abc7efd8bcc70fe41e7a84d4edc83158916f2f
colbee <laracuente.colby@gmail.com>
2022-03-08 16:45:46 -0500

    testing permit/deny conditions with wordlist

--- ZAP_Scripts/httpfuzzerprocessor/password_security.js
+++ ZAP_Scripts/httpfuzzerprocessor/password_security.js
@@ -32,6 +32,8 @@ Once the fuzzer is run with the provided wordlist, this script will check the re
 
 // Auxiliary variables/constants needed for processing.
 var count = 1;
+
+//function to determine if string contains a special character using regex
 function containsSpecial(str){
 	var regex = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
 	return regex.test(str);
@@ -43,58 +45,48 @@ function processMessage(utils, message) {
 }
 
 function processResult(utils, fuzzResult){
-	// All the above 'utils' functions are available plus:
-	// To raise an alert:
-	//    utils.raiseAlert(risk, confidence, name, description)
-	// To obtain the fuzzed message, received from the server:
-	//    fuzzResult.getHttpMessage()
-	// To get the values of the parameters configured in the Add Message Processor Dialog.
-	//    utils.getParameters() 
-	// A map is returned, having as keys the parameters names (as returned by the getRequiredParamsNames()
-	// and getOptionalParamsNames() functions below)
-	// To get the value of a specific configured script parameter
-	//    utils.getParameters().get("exampleParam1")
 
+	//testing variables
      var payload = utils.getPayloads().toString();
      var codes = [200, 201, 202, 203, 204, 205, 206, 207, 208, 209];
 
      var response_code = fuzzResult.getHttpMessage().getResponseHeader().getStatusCode();
 	var index = codes.indexOf(response_code);
+	var length = payload.length;
+  
 
-     var msg = fuzzResult.getHttpMessage();
-
-     var alertRisk= 0;
-     var alertConfidence = 1;
-     var alertTitle = "Fuzzer: Password Security";
-     var alertDescription = "Payload: " + payload + "\n" + "Status Code: " + response_code + "\n" + "2.1.7 - Verify that passwords submitted during account registration, login, and password change are checked against a set of breached passwords "
-       + "either locally (such as the top 1,000 or 10,000 most common passwords which match the system's password policy) or using an external API. "
-       + "If using an API a zero knowledge proof or other mechanism should be used to ensure that the plain text password is not sent or used in verifying the breach status of the password. ";
-       + "If the password is breached, the application must require the user to set a new non-breached password."
-       + "cweID:  " + "521"
-       + "\n" + "https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/04-Authentication_Testing/07-Testing_for_Weak_Password_Policy";
-     var length = payload.length;
+	//alert info
+     var risk= 0;
+     var confidence = 1;
+     var name = "Fuzzer: Password Security";
+     var description = "Please check the state column in the Fuzzer window to see which ASVS control was triggered for each payload." + "\n" + "Status Code: " + response_code + "\n" + "cweID:  " + "521" + "\n" + "https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/04-Authentication_Testing/07-Testing_for_Weak_Password_Policy";
      
      
 	if (index != -1){//if, status code is found in list of successful codes (permitted)
-          if (length < 12){
-               
-               fuzzResult.addCustomState("Key Custom State", "2.1.1 - Verify that user set passwords are at least 12 characters in length (after multiple spaces are combined).")
-          }else if (length > 128){
-               
+          if (length < 12){//Payload is less than 12 characters (2.1.1)
+               fuzzResult.addCustomState("Key Custom State", "2.1.1 - Verify that user set passwords are at least 12 characters in length (after multiple spaces are combined).");
+               risk= 1;
+			utils.raiseAlert(risk, confidence, name, description);
+          }else if (length > 128){ //Payload is greater than 128 characters (2.1.2)
                fuzzResult.addCustomState("Key Custom State", "2.1.2 - Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied.")
-          }else{
+               risk= 1;
+			utils.raiseAlert(risk, confidence, name, description);
+          }else{//Payload is from top 1,000 most common passwords, taken from rockyou.txt (2.1.7)
                fuzzResult.addCustomState("Key Custom State", "2.1.7 - Verify that passwords submitted during account registration, login, and password change are checked against a set of breached passwords" 
         + "either locally (such as the top 1,000 or 10,000 most common passwords which match the system's password policy) or using an external API. "
         + "If using an API a zero knowledge proof or other mechanism should be used to ensure that the plain text password is not sent or used in verifying the breach status of the password. "
         + "If the password is breached, the application must require the user to set a new non-breached password.")
+               risk= 1;
+			utils.raiseAlert(risk, confidence, name, description);
 }    
 
-     }else{//if denied/error
-          print(length);
-          if (length > 63 && length < 129){
+     }else{//if denied/error (response code is not 200-209)
+          if (length > 63 && length < 129){//Payload is between 64 and 128 characters (2.1.2) but was denied
 			fuzzResult.addCustomState("Key Custom State", "2.1.2 - Verify that passwords of at least 64 characters are permitted, and that passwords of more than 128 characters are denied.")
-		}else if ((length > 11 && length < 129) && containsSpecial(payload)){
+			utils.raiseAlert(risk, confidence, name, description);
+		}else if ((length > 11 && length < 129) && containsSpecial(payload)){//Payload is valid length and contains special character (2.1.9) but was denied
                fuzzResult.addCustomState("Key Custom State", "2.1.9 - Verify that there are no password composition rules limiting the type of characters permitted. There should be no requirement for upper or lower case or numbers or special characters.")
+			utils.raiseAlert(risk, confidence, name, description);
           }
 
      }
