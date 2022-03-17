781d1d6de4e7bece36ff7c8438dfe9970b3a0dbf
colbee <laracuente.colby@gmail.com>
2022-03-17 16:25:02 -0400

    testing and add comments

--- ZAP_Scripts/passive/4-1-5-fail-securely.py
+++ ZAP_Scripts/passive/4-1-5-fail-securely.py
@@ -1,9 +1,11 @@
 """
 
 Script testing 4.1.5 control from OWASP ASVS 4.0:
+
 'Verify that access controls fail securely including when an exception occurs.'
 
-The script will raise an alert if 
+The script will raise an alert if an error status code is returned 400-599 and 
+sensitive data is leaked in the response body like ssn, email, file_path, zip_code, ip, netid or version information
  
 """
 import re
@@ -11,7 +13,7 @@ import re
 def scan(ps, msg, src):
 
   #alert parameters
-  alertRisk= 0
+  alertRisk= 2
   alertConfidence = 1
   alertTitle = "4.1.5 Verify that access controls fail securely."
   alertDescription = "Verify that access controls fail securely including when an exception occurs."
@@ -29,21 +31,26 @@ def scan(ps, msg, src):
 
   error_pattern = re.compile(r"[4-5][0-9]{2}") #regular expression for codes 400-599
 
+  #regular expressions for sensitive data
   ssn = re.compile(r"[0-9]{3}-[0-9]{2}-[0-9]{4}")
   email = re.compile(r"^[\w\.=-]+@[\w\.-]+\.[\w]{2,3}$")
   file_path = re.compile(r"\\[^\\]+$")
   zip_code = re.compile(r"^((\d{5}-\d{4})|(\d{5})|([A-Z]\d[A-Z]\s\d[A-Z]\d))$")
   ip = re.compile(r"^\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}$")
   netid = re.compile(r"^([a-z]{2,3})([2-9]{1,5})$")
+  version = re.compile(r"[a-zA-Z]{1}\d{1,2}\.\d{1,2}\.\d{1,3}")
 
-  patterns = [ssn, email, file_path, zip_code, ip, netid]
+  patterns = [(ssn, "ssn"), (email, "email"), (file_path, "file path"), (zip_code, "zip code"), (ip, "ip address"), (netid, "netid"), (version, "version")]
 
   error_code = re.search(error_pattern,code)
 
-  if (parameters != ""):
+  #if the response code is 400-599 and loop through the list of patterns
+  #and if the response body contains one of the regex, raise an alert
+  if (error_code):
     for pat in patterns:
-      if (re.search(pat,body) and ():  
-        alertEvidence = "Code: " + code
+      match = re.search(pat[0],body)
+      if (match):  
+        alertEvidence = "Error triggered. Status Code: " + code + "\n" + "Possible " + pat[1] + " found in response body: "+ match.group(0)
         ps.raiseAlert(alertRisk, alertConfidence, alertTitle, alertDescription, 
         url, alertParam, alertAttack, alertInfo, alertSolution, alertEvidence, cweID, wascID, msg);
 
